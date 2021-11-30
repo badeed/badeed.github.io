047706ea8e485ba68757ba8f36ba99bef5868efe
muhsen <muhsen@pdx.edu>
2021-11-30 11:03:45 -0800

    Done with background experince and bit of projects

--- script.js
+++ script.js
@@ -21,6 +21,8 @@ $(document).ready(function (event) {
 		{ t: 'i|', ms: 200 },
 		{ t: 'i ', ms: 200 },
 		{ t: 'i a|', ms: 200 },
+		{ t: 'i am', ms: 200 },
+		{ t: 'i am |', ms: 200 },
 		{ t: 'i am b', ms: 200 },
 		{ t: 'i am ba|', ms: 200 },
 		{ t: 'i am bad', ms: 200 },
@@ -42,7 +44,7 @@ $(document).ready(function (event) {
 		if (i < anim.length) {
 			setTimeout(update, step.ms);
 
-			if (i === 26) {
+			if (i === 28) {
 				updateAbout();
 			}
 		}
@@ -50,7 +52,7 @@ $(document).ready(function (event) {
 	update();
 	//https://developer.mozilla.org/en-US/docs/Web/API/setTimeout
 	var about =
-		'I am on my path to becoming a software engineer. I have years of experince developing and using code. I am from Kuwait, i started programmering and coding since i was 14 years old. I got into it because i wanted to modify and edit my own games. Now I make games, and design them. I also make macros and bots that can ease tasks. I like creating new things out of nothing. Coding was able to give me that ability. Also i am a gamer my favorite game is League of Legends.';
+		'I am on my path to becoming a software engineer. I have years of experince developing and using code. I am from Kuwait, i started programmering and coding since i was 14 years old. I got into it because i wanted to modify and edit my own games. Now I make games, and design them. I also make macros and bots that can ease tasks. I like creating new things out of nothing. Coding was able to give me that ability. Also i am a gamer my favorite game is League of Legends. I am Muhsen Saleh';
 	var j = 0;
 	let updateAbout = () => {
 		if (j < about.length) {
